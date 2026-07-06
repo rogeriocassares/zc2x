4bd55e21cccb22a88b226ad7b435bcbc8e208d43
Rogério Cassares <rogeriocassares@Rogerios-MacBook-Pro-2.local>
2026-07-06 10:54:05 -0300

    add zc2x_config, zc2x_core, zc2x_logger

---
+++ firmware/obu/main/device_config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "zc2x_config.h"
+
+/*----------------------------------------------------------
+ * Device Identity
+ *---------------------------------------------------------*/
+
+#define ZC2X_DEVICE_ID 1
+
+#define ZC2X_DEVICE_NAME "OBU-001"
+
+#define ZC2X_DEVICE_ROLE ZC2X_ROLE_OBU
--- firmware/obu/main/main.c
+++ firmware/obu/main/main.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
+
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "device_config.h"
 #include "zc2x_core.h"
+#include "zc2x_logger.h"
+#include "zc2x_config.h"
 
 void app_main(void)
 {
   zc2x_core_init();
-  printf("\n");
-  printf("=========================\n");
-  printf(" ZC2X OBU\n");
-  printf(" Version 0.0.1\n");
-  printf("=========================\n");
 
-  while (1)
+  zc2x_logger_init();
+
+  zc2x_config_init(
+      ZC2X_DEVICE_ROLE,
+      ZC2X_DEVICE_ID,
+      ZC2X_DEVICE_NAME);
+
+  const zc2x_config_t *cfg = zc2x_config_get();
+
+  printf("Device ID   : %lu\n", (unsigned long)cfg->device_id);
+
+  printf("Device Name : %s\n", cfg->device_name);
+
+  printf("Role        : %d\n", cfg->role);
+
+  while (true)
   {
-    printf("OBU Alive\n");
     vTaskDelay(pdMS_TO_TICKS(1000));
   }
 }
