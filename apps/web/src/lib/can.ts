75037e7d670e0d19895f6c7e5aab13cad6c4ef9a
Pedro Correia <102699058+PedroCorreia73@users.noreply.github.com>
2026-07-20 23:09:33 -0300

    feat: set Can messages on next.js

--- apps/web/src/lib/can.ts
+++ apps/web/src/lib/can.ts
@@ -2,17 +2,230 @@
 
 import { Dispatch, SetStateAction } from "react";
 
-export function canParser(message : Uint8Array, setCanList: Dispatch<SetStateAction<Map<string, [{timestamp : number, data : number}]>>>){
+type CanData = Array<{timestamp: Number, data : Number}>
+
+type CanMap = {
+  WheelSpeedFL : CanData;
+  WheelSpeedFR : CanData;
+  WheelSpeedRL : CanData;
+  WheelSpeedRR : CanData;
+  
+  SteeringAngle : CanData;
+  GForceLateral : CanData;
+  GForceLongitudional : CanData;
+  GroundSpeed : CanData;
+
+  BrakePressureFront : CanData;
+  BrakePressureRear : CanData;
+
+  EngineRPM : CanData;
+  ThrottlePosition : CanData;
+  Lambda1 : CanData;
+  ManifoldAirPressure : CanData;
+  Gear : CanData;
+
+  EngineCoolantTemperature : CanData;
+  EngineOilTemperature: CanData;
+  ManifoldAirTemperature : CanData;
+  EngineOilPressure : CanData;
+
+  FuelLinePressure : CanData;
+  FuelUsedRaw : CanData;
+
+  ExhaustCylinderTemperature1: CanData;
+  ExhaustCylinderTemperature2: CanData;
+  ExhaustCylinderTemperature3: CanData;
+
+  Latitude : CanData;
+  Longitude : CanData;
+
+  Altitude : CanData;
+  GPSSpeed : CanData;
+}
+
+
+export function canParser(message : Uint8Array, setCanList: Dispatch<SetStateAction<CanMap>>){
   const timestamp = uint8ArrayToNumber(message, 15, 8, true);
   const canId     = uint8ArrayToNumber(message, 23, 8, true);
-  const canData   = uint8ArrayToNumber(message, 28, 8, true);
+  const canData   = message.subarray(28, 36);
   console.log(timestamp);
   console.log(canId);
   console.log(canData);
   switch(Number(canId)){
-    case 0x00000000:
+    case 256:
+      setCanList(
+        prev => {
+          prev.WheelSpeedFL.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 0, 2, true))
+          })
+          prev.WheelSpeedFR.push({
+            timestamp: Number(timestamp),
+            data : Number(uint8ArrayToNumber(canData, 2,2, true))
+          })
+          prev.WheelSpeedRL.push({
+            timestamp: Number(timestamp),
+            data : Number(uint8ArrayToNumber(canData, 4,2, true))
+          })
+          prev.WheelSpeedRR.push({
+            timestamp : Number(timestamp),
+            data : Number(uint8ArrayToNumber(canData, 6,2, true))
+          })
+          return prev;
+        }
+      )
       break;
-    case 0x00000001:
+    case 272:
+      setCanList(
+        prev => {
+          prev.SteeringAngle.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 0, 2, true))
+          })
+          prev.GForceLateral.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 2,2, true))
+          })
+          prev.GForceLongitudional.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 4,2, true))
+          })
+          prev.GroundSpeed.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 6,2, true))
+            })
+          return prev;
+        }
+      )
+      break;
+    case 288:
+      setCanList(
+        prev => {
+          prev.BrakePressureFront.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 0, 2, true))
+          })
+          prev.BrakePressureRear.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 2, 2, true))
+          })
+          return prev;
+        }
+      )
+      break;
+    case 512:
+      setCanList(
+        prev => {
+          prev.EngineRPM.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 0, 2, true))
+          })
+          prev.ThrottlePosition.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 2,1, true))
+          })
+          prev.Lambda1.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 3, 2, true))
+          })
+          prev.ManifoldAirPressure.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 5, 2, true))
+          })
+          prev.Gear.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 7, 1, true))
+          })
+          return prev;
+        }
+      )
+      break;
+    case 528:
+      setCanList(
+        prev => {
+          prev.EngineCoolantTemperature.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 0, 2, true))
+          })
+          prev.EngineOilTemperature.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 2, 4, true))
+          })
+          prev.ManifoldAirTemperature.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 4, 6, true))
+          })
+          prev.EngineOilPressure.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 6, 2, true))
+          })
+          return prev;
+        }
+      )
+      break;
+    case 529:
+      setCanList(
+        prev => {
+          prev.FuelLinePressure.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData,0,2,true))
+          })
+          prev.FuelUsedRaw.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 2, 4, true))
+          })
+          return prev;
+        }
+      )
+      break;
+    case 544:
+      setCanList(
+        prev => {
+          prev.ExhaustCylinderTemperature1.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 0,2,true))
+          })
+          prev.ExhaustCylinderTemperature2.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 2,2,true))
+          })
+          prev.ExhaustCylinderTemperature3.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 4,2,true))
+          })
+          return prev;
+        }
+      )
+      break;
+    case 768:
+      setCanList(
+        prev => {
+          prev.Latitude.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 0,4,true))
+          })
+          prev.Longitude.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 4,4,true))
+          })
+          return prev;
+        }
+      )
+      break;
+    case 784:
+      setCanList(
+        prev => {
+          prev.Altitude.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 0,2, true))
+          })
+          prev.GPSSpeed.push({
+            timestamp: Number(timestamp),
+            data: Number(uint8ArrayToNumber(canData, 2,4, true))
+          })
+          return prev;
+        }
+      )
       break;
   }
 }
